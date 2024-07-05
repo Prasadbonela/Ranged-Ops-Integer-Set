5a2b1e4a751f04faaaced3be70b570b50ae69dcf
Prasad52892 <prasadbonela52892@gmail.com>
2024-07-05 12:42:26 +0530

    Solved

--- src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
+++ src/main/java/com/epam/autotasks/collections/RangedOpsIntegerSet.java
@@ -2,34 +2,65 @@ package com.epam.autotasks.collections;
 
 import java.util.AbstractSet;
 import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-class RangedOpsIntegerSet extends AbstractSet<Integer> {
+public class RangedOpsIntegerSet extends AbstractSet<Integer> {
+
+    private final SortedSet<Integer> elements;
+
+    public RangedOpsIntegerSet() {
+        this.elements = new TreeSet<>();
+    }
 
     public boolean add(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+        boolean modified = false;
+        for (int i = fromInclusive; i < toExclusive; i++) {
+            modified |= elements.add(i);
+        }
+        return modified;
     }
 
     public boolean remove(int fromInclusive, int toExclusive) {
-        throw new UnsupportedOperationException();
+        boolean modified = false;
+        for (int i = fromInclusive; i < toExclusive; i++) {
+            modified |= elements.remove(i);
+        }
+        return modified;
     }
 
     @Override
     public boolean add(final Integer integer) {
-        throw new UnsupportedOperationException();
+        return elements.add(integer);
     }
 
     @Override
     public boolean remove(final Object o) {
-        throw new UnsupportedOperationException();
+        return elements.remove(o);
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        throw new UnsupportedOperationException();
+        return elements.iterator();
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return elements.size();
+    }
+
+    // Example usage in main method for testing purposes
+    public static void main(String[] args) {
+        RangedOpsIntegerSet set = new RangedOpsIntegerSet();
+        set.add(1, 5);
+        set.add(9, 11);
+        for (Integer el : set) {
+            System.out.println(el);
+        }
+
+        set.remove(3, 12);
+        for (Integer el : set) {
+            System.out.println(el);
+        }
     }
 }
